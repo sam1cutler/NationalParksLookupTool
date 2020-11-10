0a48b84cd774a51fc4330604d0f51fa7420c0d4c
Sam Cutler <sam.cutler@gmail.com>
2020-11-10 09:30:53 -0800

    core functionality all present, now want to implement complete state form

--- index.js
+++ index.js
@@ -1,27 +1,115 @@
 'use strict';
 
+// Define list of states with 2-letter abbreviations to make the form
+const statesList = [ ['AK','Alaska'], 
+                ['AL','Alabama'], 
+                ['AR','Arkansas'], 
+                ['AS','American Samoa'], 
+                ['AZ','Arizona'], 
+                ['CA','California'], 
+                ['CO','Colorado'], 
+                ['CT','Connecticut'], 
+                ['DC','District of Columbia'], 
+                ['DE','Delaware'], 
+                ['FL','Florida'], 
+                ['GA','Georgia'], 
+                ['GU','Guam'], 
+                ['HI','Hawaii'], 
+                ['IA','Iowa'], 
+                ['ID','Idaho'], 
+                ['IL','Illinois'], 
+                ['IN','Indiana'], 
+                ['KS','Kansas'], 
+                ['KY','Kentucky'], 
+                ['LA','Louisiana'], 
+                ['MA','Massachusetts'], 
+                ['MD','Maryland'], 
+                ['ME','Maine'], 
+                ['MI','Michigan'], 
+                ['MN','Minnesota'], 
+                ['MO','Missouri'], 
+                ['MS','Mississippi'], 
+                ['MT','Montana'], 
+                ['NC','North Carolina'], 
+                ['ND','North Dakota'], 
+                ['NE','Nebraska'], 
+                ['NH','New Hampshire'], 
+                ['NJ','New Jersey'], 
+                ['NM','New Mexico'], 
+                ['NV','Nevada'], 
+                ['NY','New York'], 
+                ['OH','Ohio'], 
+                ['OK','Oklahoma'], 
+                ['OR','Oregon'], 
+                ['PA','Pennsylvania'], 
+                ['PR','Puerto Rico'], 
+                ['RI','Rhode Island'], 
+                ['SC','South Carolina'], 
+                ['SD','South Dakota'], 
+                ['TN','Tennessee'], 
+                ['TX','Texas'], 
+                ['UT','Utah'], 
+                ['VA','Virginia'], 
+                ['VI','Virgin Islands'], 
+                ['VT','Vermont'], 
+                ['WA','Washington'], 
+                ['WI','Wisconsin'], 
+                ['WV','West Virginia'], 
+                ['WY','Wyoming'] ];
+
 // Define my apiKey (would like to find more secure solution eventually?)
 const apiKey = 'kOf6V8XbiNOXEOa7YUkCv6nq1CCkMYhgOBkTFEDP';
 
 // Define the part of the endpoint URl that will always be present
 const endpointURLstart = 'https://developer.nps.gov/api/v1/parks?';
 
+
+function displayResults(responseJson) {
+    //console.log(responseJson.data[0]);
+    
+    // Empty results list, if it has contents from a previous search.
+    $('#results-list').empty();
+
+    // Iterate through the array of parks, creating a list entry for each one
+    for (let i=0 ; i<responseJson.data.length ; i++) {
+        console.log(responseJson.data[i]);
+        $('.js-results-list').append(
+            `<li><h3>${responseJson.data[i].fullName}</h3>
+            <ul>
+                <li>Park description: ${responseJson.data[i].description}</li>
+                <li>Park website: <a href=${responseJson.data[i].url} target="_blank">${responseJson.data[i].url}</a></li>
+            </ul>
+            </li>`)
+    }
+
+    // Make "results" section visible
+    $('.results').removeClass('hidden');
+
+    // Make the "clear form" button visible
+    $('.clear-button').removeClass('hidden');
+
+}
+
 // Function to create the string containing the search parameters
 function formatQueryParams(parameters) {
     console.log('Ran formatQueryParams function');
     
     // Initialize empty queryString
-    let queryString = ''
+    let queryString = 'stateCode='
 
     // Add the state codes
     for (let i=0 ; i<parameters.states.length ; i++) {
         console.log(parameters.states[i]);
-        queryString += `stateCode=${parameters.states[i]}&`;
+        queryString += `${parameters.states[i]},`;
     }
     console.log(queryString);
+
+    queryString = queryString.slice(0,-1); // remove the final comma
+
+    console.log('sliced string is '+queryString);
     
     // Add the max results to displayu
-    queryString += `limit=${parameters.maxResults}`;
+    queryString += `&limit=${parameters.maxResults}`;
     console.log(queryString);
 
     // Add the API key
@@ -55,16 +143,19 @@ function getParkInfo(requestedStates, maxResults) {
             if (response.ok) {
                 return response.json();
             }
-            throw new Erro(response.statusText);
+            throw new Error(response.statusText);
         })
-        .then(responseJson => console.log(responseJson))
-
-
+        .then(responseJson => displayResults(responseJson))
+        .catch(err => {
+            $('#js-error-message').text(`Something went wrong: ${err.message}`);
+        });
+    
+    
 }
 
 // Function to set up the event listener on the search form submission
-function watchForm() {
-    console.log('Ran watchForm function');
+function watchFormSubmission() {
+    console.log('Ran watchFormSubmission function.');
 
     $('form').on('submit', function(event) {
         event.preventDefault();
@@ -78,9 +169,25 @@ function watchForm() {
 
         const maxResults = $('#js-max-results').val()
         console.log(maxResults);
-
+        
+        // Provide submitted info as arguments to getParkInfo function
         getParkInfo(requestedStates, maxResults);
     })
 }
 
-$(watchForm);
+// Function to set up the event listener on clearing the form
+function watchFormClear() {
+    console.log('Ran watchFormClear function.');
+
+    $('.clear-button').on('click', function(event) {
+        console.log('User requested to clear the form.');
+        event.preventDefault(); 
+    })
+}
+
+function handleLookUpPage() {
+    watchFormSubmission();
+    watchFormClear();
+}
+
+$(handleLookUpPage);
